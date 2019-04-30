626a53490fcefaaf77e8ecc5548db5fd6ed5ec0c
machuang <632190797@qq.com>
2019-04-30 14:05:28 +0800

    * 修改BrandController命名

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -12,7 +12,7 @@ import com.pinyougou.util.PageResult;
 
 @RestController
 @RequestMapping("/brand")
-public class BrandControll {
+public class BrandController {
 
 	@Reference
 	private BrandService brandService;
